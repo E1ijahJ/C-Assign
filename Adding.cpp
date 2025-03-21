ce32cfe26abbf99db6419b9ebb350ede7ae03126
E1ijahJ <elijahguijoseph@gmail.com>
2025-03-20 21:46:44 -0400

    Completed adding functions for adding file

--- Adding.cpp
+++ Adding.cpp
@@ -4,3 +4,20 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+int add_Stack(int a, int b){
+    return a+b;
+}
+
+int add_Heap(int* a, int* b){
+    int sum = *a +*b;
+    delete a;
+    delete b;
+    return sum;
+}
+
+int add_Mixed(int Snum, int* Hnum){
+    int sum = Snum + *Hnum;
+    delete Hnum;
+    return sum;
+}

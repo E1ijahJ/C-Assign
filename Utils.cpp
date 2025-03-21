9927a779b81318638de7678c1df90ad05158e379
E1ijahJ <elijahguijoseph@gmail.com>
2025-03-20 23:32:06 -0400

    Added documentaoin for said C++ file

--- Utils.cpp
+++ Utils.cpp
@@ -1,10 +1,18 @@
 #include <tuple>
-
+/// @brief  This right here takes 2 values and then swaps the vaules and retuns it as a tuple 
+/// @tparam T 
+/// @param a 
+/// @param b 
+/// @return 
 template<typename T>
 std::tuple<T, T> swap_values(T a, T b) {
     return std::make_tuple(b, a);
 }
-
+/// @brief This right here returns the larger of the two values that were given to in the function 
+/// @tparam T 
+/// @param a 
+/// @param b 
+/// @return 
 template<typename T>
 T max_value(T a, T b) {
     return (a > b) ? a : b;
